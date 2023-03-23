e9692a0c0aa430ed5b02a31303a0ab62258d4e12
ivan <ivanmarinin2002@gmail.com>
2023-03-23 20:47:20 +0300

    Доработал шаблонизацию

--- templates/index.php
+++ templates/index.php
@@ -15,27 +15,8 @@
       <h2>Открытые лоты</h2>
     </div>
     <ul class="lots__list">
-      <?php foreach ($lots as $key => $value): ?>
-      <li class="lots__item lot">
-        <div class="lot__image">
-          <img src="<?=$value['path'];?>" width="350" height="260" alt="<?=$value['hint'];?>">
-        </div>
-        <div class="lot__info">
-          <span class="lot__category"><?=$value['category'];?></span>
-          <h3 class="lot__title"><a class="text-link" href="index.php?page=lot"><?=$value['name'];?></a></h3>
-          <div class="lot__state">
-            <div class="lot__rate">
-              <span class="lot__amount">
-                <?=($value['bets'] == 0)?('Стартовая цена'):$value['bets'].' '.get_noun_plural_form($value['bets'], 'ставка', 'ставки', 'ставок')?>
-                </span>
-              <span class="lot__cost"><?=$value['price'];?><b class="rub">р</b></span>
-            </div>
-            <div class="lot__timer timer <?=($value['timer--finishing'])?('timer--finishing'):('');?>">
-            <?=$value['time'];?>
-            </div>
-          </div>
-        </div>
-      </li>
+      <?php foreach ($lots as $key => $lot): ?>
+        <?=include_template('blocks/lot.php', ['lot' => $lot])?>
       <?php endforeach; ?>
     </ul>
   </section>
